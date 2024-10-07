4a80d0f4abee9ffd3aa8ad980f54c48702260841
Ikurooo <ivanecankov@gmail.com>
2024-10-07 19:26:52 +0200

    Added java lsp :MasonInstall jdtls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -16,6 +16,33 @@ for _, lsp in ipairs(servers) do
     }
 end
 
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+lspconfig.jdtls.setup {
+    capabilities = capabilities,
+    cmd = {
+        "java",
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.protocol=true",
+        "-Dlog.level=ALL",
+        "-Xms1g",
+        "-Xmx2G",
+        "-jar",
+        "/home/ivan/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar",
+        "-configuration",
+        "/home/ivan/.local/share/nvim/mason/packages/jdtls/config_linux",
+        "-data",
+        vim.fn.stdpath "cache" .. "/jdtls-workspace",
+    },
+    root_dir = lspconfig.util.root_pattern(".git", "mvnw", "gradlew", "pom.xml", "build.gradle"),
+    settings = {
+        java = {
+        },
+    },
+}
+
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
 --   on_attach = nvlsp.on_attach,
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,71 @@ return {
         end,
     },
 
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
+            "hrsh7th/cmp-buffer", -- Buffer completions
+            "hrsh7th/cmp-path", -- File path completions
+            "hrsh7th/cmp-cmdline", -- Command-line completions
+            "L3MON4D3/LuaSnip", -- Snippet engine
+            "saadparwaiz1/cmp_luasnip", -- Snippet completions
+        },
+        config = function()
+            local cmp = require "cmp"
+            cmp.setup {
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert {
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() and cmp.get_selected_entry() then
+                            cmp.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true }
+                            vim.api.nvim_feedkeys(
+                                vim.api.nvim_replace_termcodes("<Space>", true, true, true),
+                                "n",
+                                true
+                            )
+                        else
+                            fallback()
+                        end
+                    end),
+                },
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                }, {
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+            }
+
+            -- Use buffer source for `/` (if you want autocompletion in search mode)
+            cmp.setup.cmdline("/", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = "buffer" },
+                },
+            })
+
+            -- Use cmdline & path source for ':' (command-line mode)
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = "path" },
+                }, {
+                    { name = "cmdline" },
+                }),
+            })
+        end,
+    },
+
     -- {
     -- 	"nvim-treesitter/nvim-treesitter",
     -- 	opts = {
